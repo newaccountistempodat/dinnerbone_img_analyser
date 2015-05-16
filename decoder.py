1a7e9a756942f83ea05d25af9a1c12088f290cd1
sagiksp <sagi2133.ksp@gmail.com>
2015-05-16 10:08:29 +0300

    Create decoder.py
    Python file to decode. requires pillow.

---
+++ decoder.py
@@ -0,0 +1,9 @@
+from PIL import Image
+combined = Image.open("plchldr.png")
+db = Image.open("dinnerbone.png")
+for i in range(1,428):
+    box = (0,480*(i-1),2,480*i)
+    region = db.crop(box)
+    pbox = (2*(i-1),0,2*i,480)
+    combined.paste(region, pbox)
+combined.show()
